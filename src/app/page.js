4fd94ac4183ef66aa334c5e57660744c2f2887c9
ManojaD2004 <manojadkc2004@gmail.com>
2024-07-07 04:40:06 +0530

    Now

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn Dolphin{" "}
+            Learn Dolphin Now{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
